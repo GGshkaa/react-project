e89ed67ab1ad05923c910f1e6f4be0c4a4c3ebfd
GGshkaa <ggizibrizi@mail.ru>
2022-05-17 18:48:48 +0300

    add Changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './Logo2.png';
+
+const Header = () => {
+    return <header className='header'>
+    <img src={logo} alt='text'/>
+  </header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () =>{
+    return  <div className='content'>
+    <div>
+      <img src='https://images.wallpaperscraft.ru/image/single/devushka_zontik_dozhd_151317_300x188.jpg' alt='img'/>
+      </div>
+    <div>avatar+ profile</div>
+    <div>My post</div>
+    <div>New post</div>
+    <div>Post1  </div>
+    <div>Post2</div>
+    </div>
+}
+export default Profile;
